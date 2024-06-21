5f23c7884922fcdc18e40952476eb8c53a4ba0b9
nandini0126 <nandiniyamale26@gmail.com>
2024-06-21 05:17:42 -0700

    changes

--- src/components/BlogCard/BlogCard.js
+++ src/components/BlogCard/BlogCard.js
@@ -6,20 +6,20 @@ function BlogCard({id, title, preImg, description, content, author, date, catego
     return (
         <Link className="blog-card" to={`/blog/${id}`}>
             <div className="calender">
-                <img src={Calender} className="calender-img" />
+                <img src={Calender} className="calender-img" alt="img"/>
                 <span className="date">{date}</span>
             </div>
-            <img src={preImg} className="pre-img"></img>
+            <img src={preImg} className="pre-img" alt="img" />
             <h2>{title}</h2>
             <p className="desc">{description}..</p>
 
             <div className="author">
-                <img src={author.authImg} className="auth-img" />
+                <img src={author.authImg} className="auth-img" alt="img"/>
                 <span className="auth-name">{author.name}</span>
             </div>
 
             <div className="comment">
-                <img src="https://cdn-icons-png.flaticon.com/128/134/134718.png" className="com-img" />
+                <img src="https://cdn-icons-png.flaticon.com/128/134/134718.png" className="com-img" alt="img" />
                 <input type="text" placeholder="Add a comment" className="com-box" />
             </div>
 
--- src/views/BlogView/BlogView.js
+++ src/views/BlogView/BlogView.js
@@ -11,18 +11,18 @@ function BlogView() {
   return (
     <div className="card">
       <div className="date-section">
-        <img src={Calender} className="cal-img" />
+        <img src={Calender} className="cal-img" alt="img" />
         <span className="date">{selectedBlog.date} </span>
       </div>
       <h1 className="title">{selectedBlog.title}</h1>
       <p className="desc">{selectedBlog.description}</p>
-      <img src={selectedBlog.preImg} className="img" />
+      <img src={selectedBlog.preImg} className="img" alt="img"/>
       <p className="content">{selectedBlog.content} </p>
 
       <h3 className="auth-heading">Author:</h3>
       <div className="auth-sec">
         
-        <img src={selectedBlog.author.authImg} className="author-img"/>
+        <img src={selectedBlog.author.authImg} className="author-img" alt="img"/>
         <span className="author-name">{selectedBlog.author.name} </span>
       </div>
     </div>
